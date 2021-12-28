063dfa34893b608dd12cc4cf19e12f350c4067ee
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-28 18:37:54 -0500

    Init Index
    A lot of changes. I forgot to commit each changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,185 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Layout from "../components/layout";
+import { JS5, JS6 } from "../data/challenges";
+import "@fontsource/rubik";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  font-family: Rubik;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+const Heading = styled.h1`
+  background: linear-gradient(135deg, #a9e9ff, #ffffff);
+  text-shadow: ${(props) =>
+    props.primary ? "0px 2px 17px rgba(20,94,120,0.44)" : "none"};
+  color: white;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 48px;
+  font-weight: 700;
+  position: ${(props) => (props.primary ? "initial" : "absolute")};
+  top: ${(props) => (props.primary ? "auto" : "0")};
+`;
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+const Line = styled.div`
+display: ${({ vertical }) => vertical ? "inline" : "block"};
+border: solid ${({ borderWidth }) => borderWidth ? borderWidth : "1.5px"} ${({ vertical }) => vertical ? "rgba(0, 103, 179, 0.2)" : "rgba(0, 103, 179, 0.1)"};
+background-color: ${({ vertical }) => vertical ? "rgba(0, 103, 179, 0.2)" : "rgba(0, 103, 179, 0.1)"};
+`;
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
+const Button = styled.button`
+  width: 192px;
+  height: 78px;
+  border-radius: ${(props => (props.left === true ? "20px 0px 0px 0px" : props.right === true && "0px 20px 0px 0px"))};
+  background-color: ${(props) => (props.focus ? "hsl(204.9, 100%, 17.5%)" : "hsl(205.3, 100%, 14.9%)")};
+  border: 0;
+  cursor: ${(props) => (props.focus ? "auto" : "pointer")};
+  transition: background-color 0.3s ease;
+
+& p {
+  font-size: 36px;
+  font-weight: 600;
+  transition: color 0.3s ease;
+  ${({ focus }) => focus ? `
+  background: linear-gradient(135deg,#20d4b4,#0083E1);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  ` : `color: hsl(195.7, 100%, 35.3%);`}
 }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
+ ${({ focus }) => !focus && `
+  &:hover {
+    background-color: hsl(205.3, 100%, 15.45%);
+
+    & p {
+      color: hsl(195.7, 100%, 37.3%);
+    }
+  }
+ `
+  }
+`;
+
+const TabButtonsContainer = styled.div`
+display: flex;
+`;
+
+const TabPanelsContainer = styled.div`
+display: flex;
+flex-direction: column;
+position: relative;
+padding: 20px 40px;
+row-gap: 20px;
+overflow-y: auto;
+max-height: 76.5%;
+height: 100%;
+
+&::-webkit-scrollbar {
+  width: 0.4rem;
 }
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
+&::-webkit-scrollbar-thumb {
+  width: 7.64px;
+  height: 97.63px;
+  border-radius: 4px;
+  background: #007ea7
 }
+`
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const Card = styled.div`
+width: 384px;
+height: 413px;
+border-radius: 20px;
+background: #003459;
+box-shadow: 0px 10px 14px -10px rgba(13, 51, 65, 0.47);
+`;
 
-// markup
+const HeadingShadowWorkaround = styled.div`
+position: relative;
+margin-bottom: 30px;
+`;
+
+const Challenge = styled.div`
+ h3 {
+   color: #FFFFFF;
+   font-weight: 500;
+ }
+
+ p {
+  color: #007EA7;
+  font-weight: 500;
+ }
+`
+
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+  const Separator = () => {
+    return <Line />;
+  };
 
-export default IndexPage
+  const Tabs = ({ defaultTab }) => {
+    const [currentTab, setCurrentTab] = useState(defaultTab || 0);
+
+    const Tab = ({ content, tabIndex }) => {
+      const putContent = content.map(({ name, description }, i) => (
+        <>
+          <Challenge key={i}>
+            <h3>{name}</h3>
+            <p>{description}</p>
+          </Challenge>
+          {
+            i !== content.length - 1 && <Separator vertical />
+          }
+        </>
+      ));
+
+      return currentTab === tabIndex ? <>{putContent}</> : null;
+    };
+
+    const TabButton = ({ children, setCurrentTab, focus, left, right, tabIndex }) => {
+      const handleTabClick = (tabIndex) => currentTab !== tabIndex && setCurrentTab(tabIndex);
+
+      return (
+        <Button right={right} left={left} focus={focus} onClick={() => handleTabClick(tabIndex)}>
+          <p>{children}</p>
+        </Button>
+      );
+    };
+
+    return (
+      <>
+        <TabButtonsContainer>
+          <TabButton tabIndex={0} left focus={currentTab === 0} setCurrentTab={setCurrentTab}>JS5</TabButton>
+          <Separator vertical />
+          <TabButton tabIndex={1} right focus={currentTab === 1} setCurrentTab={setCurrentTab}>JS6</TabButton>
+        </TabButtonsContainer>
+        {/* <div style={{overflow: "auto"}}> */}
+        <TabPanelsContainer>
+          <Tab tabIndex={0} content={JS5} />
+          <Tab tabIndex={1} content={JS6} />
+        </TabPanelsContainer>
+        {/* </div> */}
+      </>
+    );
+  };
+
+  return (
+    <Layout>
+      <Main>
+        <HeadingShadowWorkaround>
+          <Heading primary>My Submissions</Heading>
+          <Heading>My Submissions</Heading>
+        </HeadingShadowWorkaround>
+        <Card>
+          <Tabs defaultTab={0} />
+        </Card>
+      </Main>
+    </Layout>
+  );
+};
+
+export default IndexPage;
