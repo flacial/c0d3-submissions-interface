24b3206e280a39dc419d5b7bc2112d5d25fba6e0
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-28 18:36:31 -0500

    Create Challenges data

---
+++ src/data/challenges.js
@@ -0,0 +1,30 @@
+export const JS5 = [
+    {
+        name: "IP Geolocation",
+        description:
+            "A site to display and lists all the users locations through Google Maps API",
+    },
+    {
+        name: "Commands",
+        description:
+            "A site where users can send Terminal Commands to the server and receive the output",
+    },
+    {
+        name: "Meme Generator",
+        description:
+            "A site that uses Jimp to create an image made from the text sent by the user",
+    },
+]
+
+export const JS6 = [
+    {
+        name: "GraphQL Intro",
+        description:
+            "A gentle introduction to GraphQL with TypeDefs, Resolvers, and Queries",
+    },
+    {
+        name: "React Stars and Kanban",
+        description:
+            "An introduction to React. Refactor Stars and Kanban challenges to JSX/React.",
+    },
+]
