1dce01bb5d6f2858d0960f5582e99d42f8aba737
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-29 17:38:55 -0500

    Init Index (mess)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Layout from "../components/layout";
 import { JS5, JS6 } from "../data/challenges";
 import "@fontsource/rubik";
@@ -8,6 +8,7 @@ import MoreIcon from "/public/static/moreIcon.svg";
 import discordIcon from "../images/discordIcon.png";
 import codeIcon from "../images/codeIcon.png";
 import repoIcon from "../images/repoIcon.png";
+import { CHALLENGES_WEBSITE } from "../constants/constants";
 
 const Main = styled.main`
   width: 100%;
@@ -74,15 +75,27 @@ const TabButtonsContainer = styled.div`
 display: flex;
 `;
 
+const fadeIn = keyframes`
+from {
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+to {
+  opactiy: 1;
+}
+`
+
 const TabPanelsContainer = styled.div`
 display: flex;
 flex-direction: column;
 position: relative;
-padding: 20px 40px;
+padding: 10px 45px;
 row-gap: 20px;
 overflow-y: auto;
-max-height: 76.5%;
+max-height: 80%;
 height: 100%;
+animation: ${fadeIn} 0.5s ease;
 
 &::-webkit-scrollbar {
   width: 0.4rem;
@@ -105,7 +118,9 @@ width: 384px;
 height: 413px;
 border-radius: 20px;
 background: #003459;
-box-shadow: 0px 10px 14px -10px rgba(13, 51, 65, 0.47);
+box-shadow: 0px 0px 20px -2px rgba(15,69,89,0.47);
+position: relative;
+z-index: 20;
 `;
 
 const HeadingShadowWorkaround = styled.div`
@@ -125,6 +140,26 @@ const Challenge = styled.div`
  }
 `
 
+const AsideLinks = styled.div`
+width: 95px;
+height: 171px;
+background: #002c4b;
+box-shadow: 0px 0px 20px -2px ${({ isVisible }) => isVisible ? "rgba(15,69,89,0.47)" : "transparent"};
+position: absolute;
+bottom: 0;
+right: 0;
+border-radius: 8px;
+display: flex;
+align-items: center;
+justify-content: center;
+padding-left: 23px;
+row-gap: 10px;
+flex-direction: column;
+transform: ${({ toRight }) => toRight && toRight};
+
+transition: transform ${({ isVisible }) => isVisible ? "ease-out 0.4s" : "ease-in 0.2s"};
+`
+
 const IndexPage = () => {
   const Separator = () => {
     return <Line />;
@@ -134,10 +169,36 @@ const IndexPage = () => {
     const [currentTab, setCurrentTab] = useState(defaultTab || 0);
 
     const Tab = ({ content, tabIndex }) => {
-      const putContent = content.map(({ name, description }, i) => (
+      const ChallengeNameAndIcon = styled.div`
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      & div {
+        padding: 10px;
+        cursor: pointer;
+      }
+
+      svg {
+        transition: filter 0.3s ease;
+      }
+
+      & div:hover svg {
+        filter: invert(42%) sepia(93%) saturate(1352%) hue-rotate(138deg);
+      }
+      `
+
+      const putContent = content.map(({ name, description, path }, i) => (
         <>
           <Challenge key={i}>
-            <h3>{name}</h3>
+            <ChallengeNameAndIcon>
+              <h3>{name}</h3>
+              <div>
+                <a target="_blank" href={`${CHALLENGES_WEBSITE}/${path}`}>
+                  <ExternalLinkIcon />
+                </a>
+              </div>
+            </ChallengeNameAndIcon>
             <p>{description}</p>
           </Challenge>
           {
@@ -166,15 +227,70 @@ const IndexPage = () => {
           <Separator vertical />
           <TabButton tabIndex={1} right focus={currentTab === 1} setCurrentTab={setCurrentTab}>JS6</TabButton>
         </TabButtonsContainer>
-        {/* <div style={{overflow: "auto"}}> */}
         <TabPanelsContainer>
           <Tab tabIndex={0} content={JS5} />
           <Tab tabIndex={1} content={JS6} />
         </TabPanelsContainer>
-        {/* </div> */}
       </>
     );
   };
+  const CardContainer = styled.div`
+  position: relative;
+  `
+
+  const ToggleAsideButton = styled.button`
+  width: 29px;
+  height: 13px;
+  background-color: hsl(204.8, 100%, 14.7%);
+  box-shadow: inset 10px 0px 5px -13px;
+  border: 0;
+  border-radius: 0 4px 4px 0;
+  position: absolute;
+  right: -29px;
+  top: 50%;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+  background-color: hsl(204.8, 100%, 16.7%);
+  }
+  `
+
+  const AsideLink = styled.div`
+  width: 36px;
+  height: 35.58px;
+  border-radius: 6px;
+  background: hsl(195.5, 100%, 6.1%);
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+  `
+
+  const AsideLinksC = () => {
+    const [right, setRight] = useState("0");
+    const toRightValue = "72px";
+
+    return (
+      <AsideLinks toRight={`translateX(${right})`} isVisible={right === "72px"}>
+        <AsideLink>
+          <img src={discordIcon} href="discord icon"></img>
+        </AsideLink>
+        <AsideLink>
+          <img src={codeIcon} href="code icon"></img>
+        </AsideLink>
+        <AsideLink>
+          <img src={repoIcon} href="repo icon"></img>
+        </AsideLink>
+        <ToggleAsideButtonC setRight={setRight} right={toRightValue}>
+          <MoreIcon />
+        </ToggleAsideButtonC>
+      </AsideLinks>
+    )
+  }
+
+  const ToggleAsideButtonC = ({ setRight, children, right }) => {
+    return <ToggleAsideButton onClick={() => setRight(prevRight => prevRight === "0" ? right : "0")}>{children}</ToggleAsideButton>
+  }
 
   return (
     <Layout>
@@ -183,9 +299,26 @@ const IndexPage = () => {
           <Heading primary>My Submissions</Heading>
           <Heading>My Submissions</Heading>
         </HeadingShadowWorkaround>
-        <Card>
-          <Tabs defaultTab={0} />
-        </Card>
+        <CardContainer>
+          <Card>
+            <Tabs defaultTab={0} />
+          </Card>
+          <AsideLinksC />
+          {/* <AsideLinks>
+            <AsideLink>
+              <img src={discordIcon} href="discord icon"></img>
+            </AsideLink>
+            <AsideLink>
+              <img src={codeIcon} href="code icon"></img>
+            </AsideLink>
+            <AsideLink>
+              <img src={repoIcon} href="repo icon"></img>
+            </AsideLink>
+            <ToggleAsideButton>
+              <MoreIcon />
+            </ToggleAsideButton>
+          </AsideLinks> */}
+        </CardContainer>
       </Main>
     </Layout>
   );
