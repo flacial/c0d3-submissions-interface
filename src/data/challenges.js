c5e1c7a44cff87f950a08227a1abeafb7c7ea53f
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-29 17:34:36 -0500

    data/challenges: Add url path to each challenge

--- src/data/challenges.js
+++ src/data/challenges.js
@@ -3,16 +3,19 @@ export const JS5 = [
         name: "IP Geolocation",
         description:
             "A site to display and lists all the users locations through Google Maps API",
+        path: "p1"
     },
     {
         name: "Commands",
         description:
             "A site where users can send Terminal Commands to the server and receive the output",
+        path: "p2/commands"
     },
     {
         name: "Meme Generator",
         description:
             "A site that uses Jimp to create an image made from the text sent by the user",
+        path: "p3"
     },
 ]
 
@@ -21,10 +24,12 @@ export const JS6 = [
         name: "GraphQL Intro",
         description:
             "A gentle introduction to GraphQL with TypeDefs, Resolvers, and Queries",
+        path: "js6/graphql-intro"
     },
     {
         name: "React Stars and Kanban",
         description:
-            "An introduction to React. Refactor Stars and Kanban challenges to JSX/React.",
+            "An introduction to React. Refactor Stars and Kanban challenges to JSXReact.",
+        path: "js6/react-stars-and-kanban"
     },
 ]
