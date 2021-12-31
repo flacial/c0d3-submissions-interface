fd85e9ffb1efbd322ba9104d905722f5e223503a
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-30 19:17:12 -0500

    Componentize Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+width: 384px;
+height: 413px;
+border-radius: 20px;
+background: #003459;
+box-shadow: 0px 0px 20px -2px rgba(15,69,89,0.47);
+position: relative;
+z-index: 20;
+`;
+
+export default Card;
