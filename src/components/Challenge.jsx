b5f6121a2343605bd6377ef22b5f83387528f3bf
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-30 19:17:22 -0500

    Componentize Challenge

---
+++ src/components/Challenge.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+
+export const Challenge = styled.div`
+ h3 {
+   color: #FFFFFF;
+   font-weight: 500;
+ }
+
+ p {
+  color: #007EA7;
+  font-weight: 500;
+ }
+`
+
+export const ChallengeNameAndIcon = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+
+& div {
+  padding: 10px;
+  cursor: pointer;
+}
+
+svg {
+  transition: filter 0.3s ease;
+}
+
+& div:hover svg {
+  filter: invert(42%) sepia(93%) saturate(1352%) hue-rotate(138deg);
+}
+`
