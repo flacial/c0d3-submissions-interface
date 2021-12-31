24e58a890c3e29f43c21ddf80e433d7e4406d175
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-30 21:15:21 -0500

    Make CardContainer mobile responsive

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,6 +2,12 @@ import styled from "styled-components";
 
 const CardContainer = styled.div`
 position: relative;
+box-shadow: 0px 0px 20px -2px rgba(15,69,89,0.47);
+border-radius: 18px;
+
+@media (max-width: 600px) {
+    overflow: hidden;
+}
 `
 
 export default CardContainer;
