20fe4e362975fbfba6b04aeb4d3c8f3ee2822a16
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-30 19:17:27 -0500

    Componentize Heading

---
+++ src/components/Heading.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+const Heading = styled.h1`
+background: linear-gradient(135deg, #a9e9ff, #ffffff);
+text-shadow: ${(props) =>
+        props.primary === true ? "0px 2px 17px rgba(20,94,120,0.44)" : "none"};
+color: white;
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+font-size: 48px;
+font-weight: 700;
+position: ${(props) => (props.primary ? "initial" : "absolute")};
+top: ${(props) => (props.primary ? "auto" : "0")};
+`;
+
+const HeadingShadowWorkaround = styled.div`
+position: relative;
+margin-bottom: 30px;
+`;
+
+const HeadingComponent = ({ children }) => (
+    <HeadingShadowWorkaround>
+        <Heading primary>{children}</Heading>
+        <Heading>{children}</Heading>
+    </HeadingShadowWorkaround>
+)
+
+export default HeadingComponent;
