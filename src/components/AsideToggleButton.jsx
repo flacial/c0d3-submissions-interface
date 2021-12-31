c7a44342daf6abfeb59772b90db55290c31f0da0
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-30 19:17:07 -0500

    Componentize AsideToggleButton

---
+++ src/components/AsideToggleButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components"
+
+const StyledToggleAsideButton = styled.button`
+width: 29px;
+height: 13px;
+background-color: hsl(204.8, 100%, 14.7%);
+box-shadow: inset 10px 0px 5px -13px;
+border: 0;
+border-radius: 0 4px 4px 0;
+position: absolute;
+right: -29px;
+top: 50%;
+cursor: pointer;
+transition: background-color 0.3s ease;
+
+&:hover {
+background-color: hsl(204.8, 100%, 16.7%);
+}
+`
+
+const ToggleAsideButton = ({ setRight, children, right }) => (
+    <StyledToggleAsideButton onClick={() => setRight(prevRight => prevRight === "0" ? right : "0")}>{children}</StyledToggleAsideButton>
+)
+
+export default ToggleAsideButton;
