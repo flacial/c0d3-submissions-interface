2a5222e2426bd5277d3168b4022870f28e7d1b04
flacial <ruthemiumwithdiamond@protonmail.com>
2022-01-01 14:12:44 -0500

    Line/fix: Empty middle space caused by borders

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -4,8 +4,8 @@ const predictColor = vertical => vertical ? "rgba(0, 103, 179, 0.2)" : "rgba(0, 
 
 const Line = styled.div`
 display: ${({ vertical }) => vertical ? "inline" : "block"};
-border: solid ${({ borderWidth }) => borderWidth ? borderWidth : "1.5px"} ${({ vertical }) => predictColor(vertical)};
 background-color: ${({ vertical }) => predictColor(vertical)};
+padding: ${({vertical}) => vertical ? "0 1.5px" : "1.5px 0"}
 `;
 
 export default Line;
