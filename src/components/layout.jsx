5cf95023fdfb136a5da297c71001c70d3bf63df7
flacial <ruthemiumwithdiamond@protonmail.com>
2022-01-01 12:10:41 -0500

    Isolate GlobalStyles from Layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,43 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import styled, { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-  /*
-  Josh's Custom CSS Reset
-  https://www.joshwcomeau.com/css/custom-css-reset/
-  */
-  *, *::before, *::after {
-    box-sizing: border-box;
-  }
-  * {
-    margin: 0;
-  }
-  html, body {
-    height: 100%; 
-    background: linear-gradient(44deg, hsl(195.5,100%,44.1%), hsl(188deg 98% 47%));
-  }
-  #gatsby-focus-wrapper, #___gatsby {
-    height: 100%;
-  }
-  body {
-    line-height: 1.5;
-    -webkit-font-smoothing: antialiased;
-  }
-  img, picture, video, canvas, svg {
-    display: block;
-    max-width: 100%;
-  }
-  input, button, textarea, select {
-    font: inherit;
-  }
-  p, h1, h2, h3, h4, h5, h6 {
-    overflow-wrap: break-word;
-  }
-  #root, #__next {
-    isolation: isolate;
-  }
-`;
+import styled from "styled-components";
+import GlobalStyle from "../styles/GlobalStyles";
+import "@fontsource/rubik";
 
 const StyledLayout = styled.div`
 width: 100%;
@@ -48,18 +13,20 @@ justify-content: center;
 align-items: center;
 flex-direction: column;
 padding: 20px;
+font-display: fallback;
 `
 
 const Layout = ({ children }) => {
-  return (<>
-    <Helmet>
-      <title>C0D3 Submissions - Flacial</title>
-    </Helmet>
-    <StyledLayout>
-      <GlobalStyle theme="purple" />
-      {children}
-    </StyledLayout>
-  </>
+  return (
+    <>
+      <GlobalStyle />
+      <Helmet>
+        <title>C0D3 Submissions - Flacial</title>
+      </Helmet>
+      <StyledLayout>
+        {children}
+      </StyledLayout>
+    </>
   );
 };
 
