1c62d493d073d345bd68030c33da729c68e01bd1
flacial <ruthemiumwithdiamond@protonmail.com>
2022-01-01 12:11:30 -0500

    Create GlobalStyles

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  /*
+  Josh's Custom CSS Reset
+  https://www.joshwcomeau.com/css/custom-css-reset/
+  */
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+  * {
+    margin: 0;
+  }
+  html, body {
+    height: 100%; 
+    background: linear-gradient(44deg, hsl(195.5,100%,44.1%), hsl(188deg 98% 47%));
+  }
+  #gatsby-focus-wrapper, #___gatsby {
+    height: 100%;
+  }
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+  input, button, textarea, select {
+    font: inherit;
+  }
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+  #root, #__next {
+    isolation: isolate;
+  }
+`;
+
+export default GlobalStyle;
