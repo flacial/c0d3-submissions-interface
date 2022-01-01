f8ca12d4fba4b893bf3df0c594000e6056dcc207
flacial <ruthemiumwithdiamond@protonmail.com>
2022-01-01 14:09:07 -0500

    Add all JS5 challenges

--- src/data/challenges.js
+++ src/data/challenges.js
@@ -2,21 +2,51 @@ export const JS5 = [
     {
         name: "IP Geolocation",
         description:
-            "A site to display and lists all the users locations through Google Maps API",
+            "Get your location by your IP and see others locations with Google Map API",
         path: "p1"
     },
     {
         name: "Commands",
         description:
-            "A site where users can send Terminal Commands to the server and receive the output",
+            "Enter terminal command and get its output back",
         path: "p2/commands"
     },
     {
         name: "Meme Generator",
         description:
-            "A site that uses Jimp to create an image made from the text sent by the user",
-        path: "p3"
+            "Use JIMP to make a meme by providing your favorite lines",
+        path: "p3/memegen/api/" + "Meme Generator On Fire!"
     },
+    {
+        name: "Asset Creation",
+        description: "Edit, upload, and access files. *Files are auto deleted",
+        path: "p4"
+    },
+    {
+        name: "Chatroom using JWT Auth",
+        description: "Chat with others securely with JWT",
+        path: "p5"
+    },
+    {
+        name: "NOT WORKING - Authentication",
+        description: "Authentication SDK used across JS5 challenges to authenticate and authorize requests",
+        path: "p6"
+    },
+    {
+        name: "Image Text Extraction",
+        description: "Extract text from images using OCR",
+        path: "p7"
+    },
+    {
+        name: "Selfie Queen",
+        description: "Smile, take a snap upload it, and see others snaps",
+        path: "p8"
+    },
+    {
+        name: "Meme Chat",
+        description: "Take a snap, enter some text, and get a meme back while being able to see others memes",
+        path: "p9/memechat"
+    }
 ]
 
 export const JS6 = [
